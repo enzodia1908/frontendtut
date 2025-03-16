bc1ea1c5204f070354ba145978f25146ac3880b8
Nadia Banik <nadiabanik@hotmail.com>
2025-03-17 09:30:36 +1100

    Initial commit for device swap of code

---
+++ src/pages/Home.jsx
@@ -0,0 +1,43 @@
+import MovieCard from "../components/MovieCard";
+import { useState } from "react";
+import { searchMovies, getPopularMovies } from "../services/api.js";
+import "../css/Home.css";
+
+function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const movies = [
+        { id: 1, title: "John Wick", release_date: "2020" },
+        { id: 2, title: "Terminator", release_date: "1999" },
+        { id: 3, title: "The Matrix", release_date: "2020" },
+    ];
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+    };
+
+    return (
+        <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input
+                    type="text"
+                    placeholder="Search for movies..."
+                    className="search-input"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                />
+                <button type="submit" className="search-button">
+                    Search
+                </button>
+            </form>
+
+            <div className="movies-grid">
+                {movies.map((movie) => (
+                    <MovieCard movie={movie} key={movie.id} />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Home;
