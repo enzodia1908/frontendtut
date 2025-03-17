0de64e4c740003d99eed797b4b7b38a6033faaf2
Nadia Banik <nadiabanik@hotmail.com>
2025-03-17 19:39:41 +1100

    Add contexts

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,22 @@ import "./css/App.css";
 import MovieCard from "./components/MovieCard";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
+import { MovieProvider } from "./contexts/MovieContext";
 import Favourites from "./pages/Favourites";
 import NavBar from "./components/NavBar";
 
 function App() {
     return (
-      <div>
-        <NavBar />
+        <MovieProvider>
+            <NavBar />
 
-        <main className="main-content">
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/favourites" element={<Favourites />} />
-            </Routes>
-        </main>
-      </div>
-        
+            <main className="main-content">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/favourites" element={<Favourites />} />
+                </Routes>
+            </main>
+        </MovieProvider>
     );
 }
 
